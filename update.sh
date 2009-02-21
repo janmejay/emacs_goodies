3c885f5c9242e70b5e6e5e56566bcb8a3eb0f234
Janmejay Singh <singh.janmejay@gmail.com>
2009-02-21 16:14:14 +0530

    Added css mode

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 
 wget http://cvs.savannah.gnu.org/viewvc/*checkout*/emacs/emacs/lisp/ido.el
 wget http://nschum.de/src/emacs/window-numbering-mode/window-numbering.el
+wget http://www.garshol.priv.no/download/software/css-mode/css-mode.el
