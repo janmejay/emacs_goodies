7e48992600e59480326c8928ac2dd6e03c7c5af0
Janmejay Singh <singh.janmejay@gmail.com>
2009-03-18 11:56:01 +0530

    trying to get specific specs running -jj

--- test-runner/rspec-runner.el
+++ test-runner/rspec-runner.el
@@ -1,9 +1,17 @@
 (require 'test-runner-base)
 
-(setq rspec-executable "spec")
+(setq rspec-executable "spec -f n")
 
 (defun build-rspec-runner-command-for (file-name)
-  (concat rspec-executable " " file-name))
+  (cond ((numberp block-selection-for-rspec-buffer) (concat rspec-executable " -l " block-selection-for-rspec-buffer " " file-name))
+        (t (concat rspec-executable " " file-name))))
+
+(setq block-selection-for-rspec-buffer nil)
+
+(defun toggle-spec-block-identification-for-selective-running ()
+  (interactive)
+  (cond ((numberp block-selection-for-rspec-buffer) (setq block-selection-for-rspec-buffer nil))
+        (t (setq block-selection-for-rspec-buffer (line-number-at-pos)))))
 
 (defun test-case-rspec-make-run-command (buffer)
   (fset 'builder-for-rspec-runner-command 'build-rspec-runner-command-for)
